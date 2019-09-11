0647e55ee9d0e7226cb4f6165b6085b84abacbe2
rsudhanya <rsudhanya@gmail.com>
2019-09-11 19:28:15 +0530

    Messages can be saved using AJAX into Daatabase

--- chatting/models.py
+++ chatting/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Message(models.Model):
+    msender = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='+',
+    )
+    mreceiver = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='+',
+    )
+    mbody = models.TextField()
+    mdate = models.DateTimeField()
--- chatting/views.py
+++ chatting/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from django.http import JsonResponse
+from django.utils import timezone
+
+from .models import Message
 
 # Create your views here.
 
@@ -9,8 +12,15 @@ from django.contrib import messages
 def index(request):
     user = request.user
     if (user.is_authenticated):
-        # return HttpResponse("Hello from Chatting!!!" + user.first_name)
-        return render(request, 'chatting/index.html')
+        if(request.method == "POST" and request.is_ajax()):
+            msg = request.POST['message']
+            rc = User.objects.get(email="guido@python.org")
+            Message.objects.create(msender=user, mreceiver=rc, mbody=str(
+                msg), mdate=timezone.now())
+            return JsonResponse({"success": True}, status=200)
+        msg = Message.objects.all()
+        args = {'msg': msg}
+        return render(request, 'chatting/index.html', args)
     else:
         messages.info(request, "Please authenticate first")
         return redirect('accounts/login')
